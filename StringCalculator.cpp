a4e6577697095805bfe79d236501edb057d2b204
Advaith-Menon <47732369+Advaith-Menon@users.noreply.github.com>
2024-06-27 15:46:12 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -6,4 +6,11 @@ int StringCalculator::add(string input)
 {
   if(input.compare("")==0 || input == "0")
     return 0;
+  int sum = 0;
+  std::stringstream ss(input);
+  std::string token;
+  while (getline(ss, token, ',')) {
+        sum += std::stoi(token);
+    }
+  return sum;
 }
