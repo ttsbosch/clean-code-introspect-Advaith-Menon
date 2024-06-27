e5ab781b0d889de8e00a2b9e093be3850c54f1c2
Advaith-Menon <47732369+Advaith-Menon@users.noreply.github.com>
2024-06-27 16:41:50 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int StringCalculator::add(string input)
 {
-  if(input.compare("")==0 || input == "0")
+  if(input.compare("")==0)
     return 0;
   int sum = 0;
   std::stringstream ss(input);
