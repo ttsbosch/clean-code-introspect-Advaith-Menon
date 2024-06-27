a329b6727ad54aa559f6e38c36bd6419e6822965
Advaith-Menon <47732369+Advaith-Menon@users.noreply.github.com>
2024-06-27 16:43:32 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -27,7 +27,6 @@ TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
 
     ASSERT_EQ(result, expectedresult);
 }
-/*
 TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
     ASSERT_THROW({
         std::string input = "-1,2";
@@ -35,7 +34,7 @@ TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
        objUnderTest.add(input);
         }, std::runtime_error);
 }
-
+/*
 TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
     std::string input = "1\n2,3";
