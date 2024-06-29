d04f501d7b2cc0b645bba85a89532ad31cfded48
Advaith-Menon <47732369+Advaith-Menon@users.noreply.github.com>
2024-06-29 11:55:41 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -8,6 +8,32 @@ void StringCalculator::containsNegative(const std::string& input) {
         throw std::runtime_error("Negative numbers not allowed");
     }
 }
+std::vector<std::string> StringCalculator::split(const std::string& s, const std::string& delimiters) {
+    std::vector<std::string> tokens;
+    size_t start = 0;
+    size_t end = s.find_first_of(delimiters);
+    while (end != std::string::npos) {
+        if (end != start) {
+            tokens.push_back(s.substr(start, end - start));
+        }
+        start = end + 1;
+        end = s.find_first_of(delimiters, start);
+    }
+    if (start < s.length()) {
+        tokens.push_back(s.substr(start));
+    }
+    return tokens;
+}
+int StringCalculator::convertAndSum(const std::vector<std::string>& tokens) {
+    int sum = 0;
+    for (const std::string& token : tokens) {
+        if (!token.empty()) {
+            sum += std::stoi(token);
+        }
+    }
+    return sum;
+}
+
 int StringCalculator::add(string input)
 {
   if(input.compare("")==0)
@@ -16,10 +42,6 @@ int StringCalculator::add(string input)
   std::stringstream ss(input);
   std::string token;
   containsNegative(input);
-  char delimiter = ',';
-
-    while (getline(ss, token, delimiter)) {
-        sum += std::stoi(token);
-    }
-   return sum;
+  std::vector<std::string> tokens = split(input, ",\n");
+  return convertAndSum(tokens);
 }
