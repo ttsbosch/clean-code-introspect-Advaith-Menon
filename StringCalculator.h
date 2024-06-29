a41d0d4bccd62d04ac3cf36173ffc749e4f8b891
Advaith-Menon <47732369+Advaith-Menon@users.noreply.github.com>
2024-06-29 11:57:44 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,4 +6,6 @@ class StringCalculator
    public:
    int add(string input);
    void containsNegative(const std::string& input);
+   std::vector<std::string> split(const std::string& s, const std::string& delimiters);
+   int convertAndSum(const std::vector<std::string>& tokens);
 };
