665cebd9df3f7f381fbccf0bad802230218f16ea
Advaith-Menon <47732369+Advaith-Menon@users.noreply.github.com>
2024-06-29 12:06:28 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -11,22 +11,19 @@ void StringCalculator::containsNegative(const std::string& input) {
 std::vector<std::string> StringCalculator::split(const std::string& s, const std::string& delimiters) {
     std::vector<std::string> tokens;
     size_t start = 0;
-    size_t end = s.find_first_of(delimiters);
-    while (end != std::string::npos) {
-        if (end != start) {
-            tokens.push_back(s.substr(start, end - start));
-        }
+    size_t end;
+    
+    while ((end = s.find_first_of(delimiters, start)) != std::string::npos) {
+        tokens.emplace_back(s.substr(start, end - start));
         start = end + 1;
-        end = s.find_first_of(delimiters, start);
     }
-    if (start < s.length()) {
-        tokens.push_back(s.substr(start));
-    }
+    
+    tokens.emplace_back(s.substr(start));
     return tokens;
 }
 int StringCalculator::convertAndSum(const std::vector<std::string>& tokens) {
     int sum = 0;
-    for (const std::string& token : tokens) {
+    for (const auto& token : tokens) {
         if (!token.empty()) {
             sum += std::stoi(token);
         }
